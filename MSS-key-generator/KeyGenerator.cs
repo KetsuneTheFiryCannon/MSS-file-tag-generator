af44cf65f51408f86a1e5a338816ec7af7ca4472
Ketsune <sasha.yakunchev@yandex.ru>
2023-01-30 19:19:44 +0300

    Git init

---
+++ MSS-key-generator/KeyGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MSS_key_generator;
+
+public class KeyGenerator
+{
+    String[] myArr = new String[]
+    {
+        "DF", "BF"
+    };
+    
+    //Activate, Deactivate, Delete, Put, Create
+    
+    //Activate, Deactivate, Delete, Read, Update, Write
+
+    //createTag-sizeTag-patternTag-sizeTag-sizeFileTag-sizeFileValue-typeFileTag-0101-
+    
+    public void generateCreateKey()
+    {
+        string createTag = "00e0000";
+        string patternTag = "62";
+    }
+}
