9d8c04e1731431124ea5134384aade49ad0a38b4
Timkin Maxim <enderman.2016@mail.ru>
2023-02-01 09:18:29 +0300

    use int for Length

--- MSS-key-generator/Backend/TLV/TagLengthValue.cs
+++ MSS-key-generator/Backend/TLV/TagLengthValue.cs
@@ -4,22 +4,20 @@ namespace MSS_key_generator.Backend.TLV;
 
 public class TagLengthValue : TagValue
 {
-    public TagLengthValue(byte tag, byte[] length, byte[] value) : base(tag, value)
+    public TagLengthValue(byte tag, byte[] value) : base(tag, value)
     {
-        Length = length;
     }
 
-    public TagLengthValue(byte tag, byte[] length, TagValue value) : base(tag, value)
+    public TagLengthValue(byte tag, TagValue value) : base(tag, value)
     {
-        Length = length;
     }
 
-    public byte[] Length { get; }
+    public int Length => Value.ComputeLength();
 
     public override string ToHex()
     {
         var hexTag = Tag.ToString("X2");
-        var hexLength = Convert.ToHexString(Length);
+        var hexLength = Length.ToString("X2");
         var hexValue = Value.ToHex();
         var hexString = string.Concat(hexTag, hexLength, hexValue);
         return hexString;
