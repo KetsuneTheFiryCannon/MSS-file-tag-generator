502f3ae372712514fa01a33dbaad20530911a8e3
Timkin Maxim <enderman.2016@mail.ru>
2023-02-01 09:11:07 +0300

    tag value compute length tests

--- TestProject1/TagValueTests.cs
+++ TestProject1/TagValueTests.cs
@@ -16,7 +16,7 @@ public class TagValueTests
 
         Assert.That(hexValue, Is.EqualTo(dataHex));
     }
-    
+
     [Test]
     public void TagValueToHex_0x80TV82010a_80010a()
     {
@@ -28,5 +28,33 @@ public class TagValueTests
         var hexValue = tagValue.ToHex();
 
         Assert.That(hexValue, Is.EqualTo(dataHex));
+    }
+
+    [Test]
+    public void TagValueComputeLength_TV0x8042_2()
+    {
+        const int expectedLength = 2;
+        const byte tagByte = 0x80;
+        const byte valueByte = 0x42;
+
+        var tagValue = new TagValue(tagByte, new[] { valueByte });
+        var computedLength = tagValue.ComputeLength();
+
+        Assert.That(computedLength, Is.EqualTo(expectedLength));
+    }
+
+    [Test]
+    public void TagValueComputeLength_TV0x80TV0x8142_2()
+    {
+        const int expectedLength = 3;
+        const byte tagByte = 0x80;
+        const byte innerTagByte = 0x81;
+        const byte valueByte = 0x11;
+
+        var innerTagValue = new TagValue(innerTagByte, new[] { valueByte });
+        var tagValue = new TagValue(tagByte, innerTagValue);
+        var computedLength = tagValue.ComputeLength();
+
+        Assert.That(computedLength, Is.EqualTo(expectedLength));
     }
 }
