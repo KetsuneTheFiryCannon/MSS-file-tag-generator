a8ad386043ebb808e9e24c0a42891eedb890a9d0
Timkin Maxim <enderman.2016@mail.ru>
2023-02-01 09:50:29 +0300

    test int to hex on small num

--- TestProject1/CommonTests.cs
+++ TestProject1/CommonTests.cs
@@ -7,10 +7,21 @@ public class CommonTests
     {
         var bytes = new byte[] { 255, 255, 255, 255 };
         const uint len = uint.MaxValue;
-        
+
         var hexInt = len.ToString("X2");
         var hexBytes = Convert.ToHexString(bytes);
-        
+
         Assert.That(hexBytes, Is.EqualTo(hexInt));
+    }
+
+    [Test]
+    public void ToHex_int1_01()
+    {
+        const string hexExpected = "01";
+        const int i = 1;
+
+        var hexActual = i.ToString("X2");
+
+        Assert.That(hexActual, Is.EqualTo(hexExpected));
     }
 }
