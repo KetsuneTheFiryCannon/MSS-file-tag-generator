bc0128887e5fff003f15fdc9d2006501a667090d
WizMe-M <timkin.moxim@mail.ru>
2023-02-02 11:33:05 +0300

    GenerationData.cs for KeyGenerator.cs

---
+++ MSS-key-generator/Backend/KeyGenerator.cs
@@ -0,0 +1,21 @@
+﻿namespace MSS_key_generator.Backend;
+
+public class KeyGenerator
+{
+    public GenerationData GenerateCreateFile(string name, FileType fileType, int size)
+    {
+        if (name is not [var first, var second])
+            throw new InvalidOperationException("Name should be two characters' length");
+
+        var command = new CreateFileCommand(
+            new FcpDataTag(new TagValue[]
+            {
+                new FileTypeTag(fileType),
+                new FileNameTag(first, second),
+                new FileSizeTag(size),
+                // TODO: access rule by file types - new AccessRuleTag(new byte[] { 0 })
+            })
+        );
+        return new GenerationData(command);
+    }
+}
