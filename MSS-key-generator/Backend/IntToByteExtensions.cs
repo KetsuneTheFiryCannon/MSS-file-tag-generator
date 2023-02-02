e01037e2e43a44696e08f8b79560fe494a6833ec
Timkin Maxim <enderman.2016@mail.ru>
2023-02-02 23:13:06 +0300

    fixed conversion from int to bytes
    there were leading zeros in array

--- MSS-key-generator/Backend/IntToByteExtensions.cs
+++ MSS-key-generator/Backend/IntToByteExtensions.cs
@@ -1,11 +1,21 @@
+using System.Linq;
+
 namespace MSS_key_generator.Backend;
 
 public static class IntToByteExtensions
 {
-    public static byte[] ToBytes(this int i)
+    public static byte[] ToBytes(this int number)
     {
-        var bytes = BitConverter.GetBytes(i);
+        var bytes = BitConverter.GetBytes(number);
         if (BitConverter.IsLittleEndian) Array.Reverse(bytes);
-        return bytes;
+
+        var lastZero = 0;
+        foreach (var b in bytes)
+        {
+            if (b == 0) lastZero++;
+            else break;
+        }
+
+        return bytes.Skip(lastZero).ToArray();
     }
 }
